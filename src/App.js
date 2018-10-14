763fb8522d53e00930823eb3bc7f9494406fc19b
Rodrigo De la Flor <rodrigo.120894@gmail.com>
2018-10-13 23:35:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Title from './components/Title';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+
+	}
+
+	render() {
+		return (
+			<section>
+				<div>
+					<div>
+						<Title title={"Places"} subtitle={"Descubre lugares de manera simple"}/>
+						<img src={process.env.PUBLIC_URL + '/img/logo.png'} width='60' height='60' alt=""/>
+						<h2>{this.state.numero}</h2>
+						<button>Crear cuenta gratuita
+						</button>
+						<div>
+							<ul>
+								<li>
+									<h3>Calificaciones con emociones</h3>
+									<p>Califica tus lugares con experencias, no con números</p>
+								</li>
+								<li>
+									<h3>¿Sin Internet? Sin problemas</h3>
+									<p>Places funciona sin internet o con conexiones lentas</p>
+								</li>
+								<li>
+									<h3>Tus lugares favoritos</h3>
+									<p>Define tu lista de sitios favoritos</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default App;
